280d6de70492c653afacb9a9658f6253ce75e75b
merysvnd00 <meryemssevindik06@gmail.com>
2025-10-02 11:42:37 +0300

    Add files via upload

---
+++ buttonsurukleme/buttonsurukleme/WindowsFormsApp31/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp31
+{
+    public partial class Form1 : Form
+    {
+        private bool dragging = false;
+        private Point startpoint = new Point(0, 0);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_MouseDown(object sender, MouseEventArgs e)
+        {
+            dragging = true;
+            startpoint = e.Location;
+        }
+
+        private void button1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (dragging)
+            {
+                button1.Left += e.X - startpoint.X;
+                button1.Top += e.Y - startpoint.Y;
+            }
+        }
+
+        private void button1_MouseUp(object sender, MouseEventArgs e)
+        {
+            dragging = false;
+        }
+    }
+}
